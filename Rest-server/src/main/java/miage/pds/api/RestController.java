2e03d4870e6e36659307abd461527e1d66127a99
Muthu Balabascarin <muthukumarasamy.balabascarin@etu.u-pec.fr>
2015-12-16 16:05:13 +0100

    modified rest home page

--- Rest-server/src/main/java/miage/pds/api/RestController.java
+++ Rest-server/src/main/java/miage/pds/api/RestController.java
@@ -45,14 +45,7 @@ public class RestController {
 	public String home(Locale locale, Model model) {
 
 
-		logger.info("Welcome home! The client locale is {}.", locale);
-
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-
-		String formattedDate = dateFormat.format(date);
-
-		model.addAttribute("serverTime", formattedDate );
+		logger.info("REST SERVER IS RUNNING :)");
 
 		return "REST SERVER IS RUNNING :)";
 	}
