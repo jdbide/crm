133195c775b0a6cf988721733d9e796fa8841493
Julien Datour <julien.datour@gmail.com>
2015-12-18 13:53:11 +0100

    Coaching

--- android-crmtab/app/src/main/java/pds/isintheair/fr/crm_tab/uc/phone/call/receive/controller/MessageController.java
+++ android-crmtab/app/src/main/java/pds/isintheair/fr/crm_tab/uc/phone/call/receive/controller/MessageController.java
@@ -2,7 +2,7 @@ package pds.isintheair.fr.crm_tab.uc.phone.call.receive.controller;
 
 import pds.isintheair.fr.crm_tab.uc.phone.call.receive.model.entity.Message;
 
-public class MessageController {
+public class    MessageController {
     public static void handleMessage(Message message) {
         switch (message.getMessageMeta().getMessageType()) {
             case CALL_OK:
