8a1f5731852c8e5895834666354acebc44409b0f
Davide Truong <cminh.truong@gmail.com>
2015-12-20 16:05:28 +0100

    Prospect 4.05 PM 20/12/2015

--- Rest-server/src/main/java/miage/pds/prospect/controller/ProspectController.java
+++ Rest-server/src/main/java/miage/pds/prospect/controller/ProspectController.java
@@ -1,8 +1,32 @@
 package miage.pds.prospect.controller;
 
+import miage.pds.prospect.dao.ProspectDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by Truong on 12/20/2015.
  */
+
+
 public class ProspectController {
-    
+    static final Logger logger = LoggerFactory.getLogger(ProspectController.class);
+
+    public static void main(String[] args) {
+        logger.info("Bootstrapping MongoDemo application");
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("webapp/WEB-INF/spring/AppServlet/servlet-context.xml");
+        ProspectDAO prospectDAO = context.getBean(ProspectDAO.class);
+
+        prospectDAO.dropCollection();
+        prospectDAO.createCollection();
+
+        for (int i = 0; i< 20; i++){
+            prospectDAO.insertProspectWithRandomPlace();
+        }
+
+        prospectDAO.findAllProspect();
+        logger.info("Finished MongoDB demo");
+    }
 }
--- Rest-server/src/main/java/miage/pds/prospect/model/Sales.java
+++ Rest-server/src/main/java/miage/pds/prospect/model/Sales.java
@@ -1,7 +1,65 @@
 package miage.pds.prospect.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 /**
  * Created by Truong on 12/20/2015.
+ * @version 1.1
+ * @since 20/12/2015
+ * @see Sales model
  */
+
+@Document(collection = "sale")
 public class Sales {
+
+    @Id
+    private Integer id;
+
+    @Indexed(name = "idProspect", unique = true)
+    private Prospect idProspect;
+
+    private double salesValue;
+
+
+    @PersistenceConstructor
+    public Sales(Prospect idProspect, double salesValue) {
+        setIdProspect(idProspect);
+        setSalesValue(salesValue);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Prospect getIdProspect() {
+        return idProspect;
+    }
+
+    public void setIdProspect(Prospect idProspect) {
+        this.idProspect = idProspect;
+    }
+
+    public double getSalesValue() {
+        return salesValue;
+    }
+
+    public void setSalesValue(double salesValue) {
+        this.salesValue = salesValue;
+    }
+
+    @Override
+    public String toString() {
+        return "Sales{" +
+                "id=" + id +
+                ", idProspect=" + idProspect +
+                ", salesValue=" + salesValue +
+                '}';
+    }
 }
