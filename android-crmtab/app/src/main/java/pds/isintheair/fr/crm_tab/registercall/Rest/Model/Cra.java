43a28a320e51100d65dab823993cd56beff65e70
BIDE <bide26@gmail.com>
2015-12-22 21:01:33 +0100

    added model Cra

---
+++ android-crmtab/app/src/main/java/pds/isintheair/fr/crm_tab/registercall/Rest/Model/Cra.java
@@ -0,0 +1,29 @@
+package pds.isintheair.fr.crm_tab.registercall.Rest.Model;
+
+/**
+ * Created by j-d on 22/12/2015.
+ */
+public class Cra {
+
+    private int iduser;
+    private int idclient;
+    private int idcontact;
+    private String clientname;
+    private String contactname;
+    private String comments;
+    private String subject;
+    private String date;
+    private int duration;
+
+    public Cra(int iduser, int idclient, int idcontact, String clientname, String contactname, String comments, String subject, String date, int duration) {
+        this.iduser = iduser;
+        this.idclient = idclient;
+        this.idcontact = idcontact;
+        this.clientname = clientname;
+        this.contactname = contactname;
+        this.comments = comments;
+        this.subject = subject;
+        this.date = date;
+        this.duration = duration;
+    }
+}
