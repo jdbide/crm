dd88fd46a2ef2598c3ee8946f1e0b425df4e8d55
BIDE <bide26@gmail.com>
2015-12-23 01:32:30 +0100

    client rest ok

--- android-crmtab/app/src/main/java/pds/isintheair/fr/crm_tab/registercall/Rest/CreateCraResponse.java
+++ android-crmtab/app/src/main/java/pds/isintheair/fr/crm_tab/registercall/Rest/CreateCraResponse.java
@@ -4,4 +4,14 @@ package pds.isintheair.fr.crm_tab.registercall.Rest;
  * Created by j-d on 22/12/2015.
  */
 public class CreateCraResponse {
+
+    private Boolean status;
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
 }
