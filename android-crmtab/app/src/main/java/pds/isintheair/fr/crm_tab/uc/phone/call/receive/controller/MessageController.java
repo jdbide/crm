47deee9a3b1a042f664a802c9bfab5c272901703
Julien Datour <julien.datour@gmail.com>
2015-12-27 15:43:04 +0100

    Tablet now handles phone call received

--- android-crmtab/app/src/main/java/pds/isintheair/fr/crm_tab/uc/phone/call/receive/controller/MessageController.java
+++ android-crmtab/app/src/main/java/pds/isintheair/fr/crm_tab/uc/phone/call/receive/controller/MessageController.java
@@ -2,11 +2,14 @@ package pds.isintheair.fr.crm_tab.uc.phone.call.receive.controller;
 
 import pds.isintheair.fr.crm_tab.uc.phone.call.receive.model.entity.Message;
 
-public class    MessageController {
+public class MessageController {
     public static void handleMessage(Message message) {
         switch (message.getMessageMeta().getMessageType()) {
             case CALL_OK:
                 CallController.call(message.getCall().getPhoneNumber());
+                break;
+            case CALL_RECEIVED:
+                CallController.notifyCallReceived(message.getCall().getPhoneNumber());
                 break;
         }
     }
--- android-crmtab/app/src/main/java/pds/isintheair/fr/crm_tab/uc/phone/call/receive/util/enumeration/MessageType.java
+++ android-crmtab/app/src/main/java/pds/isintheair/fr/crm_tab/uc/phone/call/receive/util/enumeration/MessageType.java
@@ -3,6 +3,7 @@ package pds.isintheair.fr.crm_tab.uc.phone.call.receive.util.enumeration;
 public enum MessageType {
     CALL,
     CALL_END,
+    CALL_RECEIVED,
     CALL_OK,
     CALL_KO,
     REGISTER_PHONE,
