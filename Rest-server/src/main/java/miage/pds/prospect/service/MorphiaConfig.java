3bc1b2485f67b22ced4930056de01150fc0ee896
Davide Truong <cminh.truong@gmail.com>
2015-12-31 21:01:22 +0100

    Change configuration of morphia mongodb

---
+++ Rest-server/src/main/java/miage/pds/prospect/service/MorphiaConfig.java
@@ -0,0 +1,9 @@
+package miage.pds.prospect.service;
+
+/**
+ * Created by Truong on 12/31/2015.
+ */
+public interface MorphiaConfig {
+    static final String URL = "localhost";
+    static final int PORT = 27017;
+}
