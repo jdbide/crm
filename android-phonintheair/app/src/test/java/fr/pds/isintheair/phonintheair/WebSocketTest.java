c66360938896eb87c26b15029d2ca1da6ef036f6
Julien Datour <julien.datour@gmail.com>
2016-01-01 10:53:08 +0100

    no message

--- android-phonintheair/app/src/test/java/fr/pds/isintheair/phonintheair/WebSocketTest.java
+++ android-phonintheair/app/src/test/java/fr/pds/isintheair/phonintheair/WebSocketTest.java
@@ -3,10 +3,14 @@ package fr.pds.isintheair.phonintheair;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import fr.pds.isintheair.phonintheair.websocket.WebSocketConnectionHandlerSingleton;
 
-
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
 public class WebSocketTest {
     @Before
     public void setUp() throws Exception {
