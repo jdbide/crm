26e2f8986bb3b86aaea68d7e2b38f5d16b773dfd
Julien Datour <julien.datour@gmail.com>
2016-01-03 17:06:29 +0100

    Nothing more, just IDE modifying files

---
+++ android-phonintheair/app/src/test/java/fr/pds/isintheair/phonintheair/controller/CallControllerTest.java
@@ -0,0 +1,21 @@
+package fr.pds.isintheair.phonintheair.controller;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.DEFAULT, sdk = 21, packageName = "fr.pds.isintheair")
+public class CallControllerTest extends TestCase {
+
+    @Test
+    public void testCall() throws Exception {
+    }
+
+    public void testEndCall() throws Exception {
+
+    }
+}
