e6a3687f1609fa0c44cb437a88e9b787f2a96df1
tlacouque <titouanlac77@gmail.com>
2016-01-17 18:09:34 +0100

    customer Dao unit test

---
+++ Rest-server/src/test/java/api/admin/customer/crud/createhc/HealthCenterDAOTest.java
@@ -0,0 +1,43 @@
+package api.admin.customer.crud.createhc;
+
+import com.mongodb.MongoClient;
+import miage.pds.api.admin.customer.crud.createhc.dao.HealthCenterDAO;
+import miage.pds.api.admin.customer.crud.createhc.entities.HealthCenter;
+import miage.pds.prospect.controller.ProspectDAOImpl;
+import miage.pds.prospect.model.Prospect;
+import org.junit.Before;
+import org.junit.Test;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by tlacouque on 17/01/2016.
+ */
+public class HealthCenterDAOTest {
+
+    MongoClient mongoClient;
+    Morphia morphia;
+    Datastore datastore;
+    HealthCenterDAO healthCenterDAO;
+    String dbName = "crm";
+
+
+    @Before
+    public void setUp() throws Exception {
+        this.mongoClient    = new MongoClient();
+        this.morphia        = new Morphia();
+        this.morphia.map(Prospect.class);
+        this.datastore      = this.morphia.createDatastore(mongoClient,dbName);
+        healthCenterDAO         = new HealthCenterDAO(datastore);
+    }
+
+    @Test
+    public void testfindAllWithoutUserId() throws Exception {
+        List<HealthCenter> healthCenters = healthCenterDAO.findAllWithoutUserId(1);
+        assertNotNull(healthCenters);
+    }
+}
