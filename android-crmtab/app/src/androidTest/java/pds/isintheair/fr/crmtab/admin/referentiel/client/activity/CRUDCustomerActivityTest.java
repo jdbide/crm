281900eb1aad38ef01e7e1295c67618bacbfe332
tlacouque <titouanlac77@gmail.com>
2016-01-19 00:00:27 +0100

    added TU

---
+++ android-crmtab/app/src/androidTest/java/pds/isintheair/fr/crmtab/admin/referentiel/client/activity/CRUDCustomerActivityTest.java
@@ -0,0 +1,7 @@
+package pds.isintheair.fr.crmtab.admin.referentiel.client.activity;
+
+/**
+ * Created by tlacouque on 18/01/2016.
+ */
+public class CRUDCustomerActivityTest {
+}
---
+++ android-crmtab/app/src/androidTest/java/pds/isintheair/fr/crmtab/admin/referentiel/client/create/he/CreateHCFragmentTest.java
@@ -0,0 +1,7 @@
+package pds.isintheair.fr.crmtab.admin.referentiel.client.create.he;
+
+/**
+ * Created by tlacouque on 18/01/2016.
+ */
+public class CreateHCFragmentTest {
+}
