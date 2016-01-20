7c5b063b6355ccb98e97095544d4f89504950ae6
Julien Datour <julien.datour@gmail.com>
2016-01-20 22:46:58 +0100

    Modified mobiles URL so that it uses prod environnement

--- android-phonintheair/app/src/main/java/fr/pds/isintheair/phonintheair/util/Constant.java
+++ android-phonintheair/app/src/main/java/fr/pds/isintheair/phonintheair/util/Constant.java
@@ -1,6 +1,6 @@
 package fr.pds.isintheair.phonintheair.util;
 
 public class Constant {
-    //public static String WS_URL = "ws://192.168.20.3:8090";
-    public static String WS_URL = "ws://192.168.1.8:8090";
+    public static String WS_URL = "ws://192.168.20.3:8090";
+    //public static String WS_URL = "ws://192.168.1.8:8090";
 }
