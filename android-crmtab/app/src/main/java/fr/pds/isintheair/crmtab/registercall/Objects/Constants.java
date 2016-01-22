02a1d33ca6aa52a8deb7b74f7532ae26f657d477
Jean-Daniel BIDE <bide26@gmail.com>
2016-01-22 10:48:43 +0100

    updated url

--- android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/registercall/Objects/Constants.java
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/registercall/Objects/Constants.java
@@ -30,7 +30,7 @@ public class Constants {
         popupdisplayed = false;
         //BASE_URL = "http://192.168.43.131:8080/api/";
         //BASE_URL = "http://192.168.1.68:8080/api/";
-        //BASE_URL = "http://192.168.20.3:8070/api/";
+        BASE_URL = "http://192.168.20.3:8070/api/";
         pendigCallList = new ArrayList<CallEndedEvent>();
         CraListForUser = new ArrayList<Cra>();
 
