5262d2d7df29ab66571fc5c7f60dbed3d42e1c6e
Julien Datour <julien.datour@gmail.com>
2016-01-22 15:48:44 +0100

    Fixed a bug preventing websocket to connect

--- android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/common/view/activity/AbstractActivity.java
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/common/view/activity/AbstractActivity.java
@@ -4,13 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import butterknife.ButterKnife;
-import pds.isintheair.fr.crmtab.R;
 
 public class AbstractActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_abstract);
         ButterKnife.bind(this);
     }
 }
