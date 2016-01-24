74944dc584b519ccb29836f422ed98f99d144e1c
Davide Truong <cminh.truong@gmail.com>
2016-01-24 16:41:35 +0100

    Change websocket uri

--- android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/ctruong/uc/propsect/suggestion/model/config/WebsocketConfig.java
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/ctruong/uc/propsect/suggestion/model/config/WebsocketConfig.java
@@ -4,5 +4,5 @@ package fr.pds.isintheair.crmtab.ctruong.uc.propsect.suggestion.model.config;
  * Created by Truong on 1/24/2016.
  */
 public interface WebsocketConfig {
-    static final String WSURI = "ws://192.168.0.11:8090/prospect";
+    static final String WSURI = "ws://192.168.20.3:8090/prospect";
 }
