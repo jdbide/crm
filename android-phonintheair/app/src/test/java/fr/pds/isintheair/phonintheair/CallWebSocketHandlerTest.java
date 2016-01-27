a3827dee28a062453cf77b566b1b738b656a5275
jdatour <julien.datour@gmail.com>
2016-01-27 01:04:42 +0100

    And finally a passing test with powermockito

--- android-phonintheair/app/src/test/java/fr/pds/isintheair/phonintheair/CallWebSocketHandlerTest.java
+++ android-phonintheair/app/src/test/java/fr/pds/isintheair/phonintheair/CallWebSocketHandlerTest.java
@@ -1,12 +1,32 @@
 package fr.pds.isintheair.phonintheair;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
-import static junit.framework.Assert.assertTrue;
+import fr.pds.isintheair.phonintheair.controller.MessageController;
+import fr.pds.isintheair.phonintheair.helper.SharedPreferencesHelper;
+import fr.pds.isintheair.phonintheair.model.websocket.CallWebSocketHandler;
 
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({MessageController.class, SharedPreferencesHelper.class})
 public class CallWebSocketHandlerTest {
     @Test
     public void testRegisterCalledOnOpen() {
-        assertTrue(false);
+        PowerMockito.mockStatic(MessageController.class);
+        PowerMockito.mockStatic(SharedPreferencesHelper.class);
+
+        PowerMockito.when(SharedPreferencesHelper.readInteger(anyString(), anyInt())).thenReturn(42);
+
+        CallWebSocketHandler callWebSocketHandler = new CallWebSocketHandler();
+        callWebSocketHandler.onOpen();
+
+        PowerMockito.verifyStatic();
+        MessageController.sendRegisterMessage(anyInt());
     }
 }
