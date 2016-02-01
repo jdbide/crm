b12e0307db078535b245e7a292ab8205b87c451e
tlacouque <titouanlac77@gmail.com>
2016-02-01 18:49:39 +0100

    #DETAILINDEPENDANT : hotfix for the map

--- android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/tlacouque/uc/admin/ref/customer/view/fragment/DetailIndepFragment.java
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/tlacouque/uc/admin/ref/customer/view/fragment/DetailIndepFragment.java
@@ -1,18 +1,28 @@
 package fr.pds.isintheair.crmtab.tlacouque.uc.admin.ref.customer.view.fragment;
 
+import android.Manifest;
 import android.app.Fragment;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -30,6 +40,8 @@ public class DetailIndepFragment extends Fragment {
 
     //Used to have the same key to pass independant from customer list view holder to this fragment
     public static final String KEY_INDEP_ARGS = "INDEP";
+    final private int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 124;
+
     @Bind(R.id.detail_indep_fragment_name)
     TextView name;
     @Bind(R.id.detail_indep_fragment_siret_number)
@@ -74,8 +86,8 @@ public class DetailIndepFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-
+        if (Build.VERSION.SDK_INT >= 23) {
+            checkPermissions();
         }
     }
 
@@ -148,6 +160,68 @@ public class DetailIndepFragment extends Fragment {
         intentWeb.setData(Uri.parse(url));
         startActivity(intentWeb);
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS: {
+                Map<String, Integer> perms = new HashMap<>();
+                // Initial
+                perms.put(Manifest.permission.ACCESS_FINE_LOCATION, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
+                // Fill with results
+                for (int i = 0; i < permissions.length; i++)
+                    perms.put(permissions[i], grantResults[i]);
+                // Check for ACCESS_FINE_LOCATION and WRITE_EXTERNAL_STORAGE
+                Boolean location = perms.get(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+                Boolean storage = perms.get(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+                if (location && storage) {
+                    // All Permissions Granted
+                    Toast.makeText(getActivity().getApplicationContext(), "All permissions granted", Toast.LENGTH_SHORT).show();
+                } else if (location) {
+                    Toast.makeText(getActivity().getApplicationContext(),
+                            "Storage permission is required to store map tiles to reduce data usage and for offline usage.",
+                            Toast.LENGTH_LONG).show();
+                } else if (storage) {
+                    Toast.makeText(getActivity().getApplicationContext(),
+                            "Location permission is required to show the user's location on map.",
+                            Toast.LENGTH_LONG).show();
+                } else { // !location && !storage case
+                    // Permission Denied
+                    Toast.makeText(getActivity().getApplicationContext(),
+                            "Storage permission is required to store map tiles to reduce data usage and for offline usage." +
+                                    "\nLocation permission is required to show the user's location on map.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+            break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+    }
+
+    private void checkPermissions() {
+        List<String> permissions = new ArrayList<>();
+        String       message     = "OSMDroid permissions:";
+        if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            message += "\nStorage access to store map tiles.";
+        }
+        if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            message += "\nLocation to show user location.";
+        }
+        if (!permissions.isEmpty()) {
+            Toast.makeText(getActivity().getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            String[] params = permissions.toArray(new String[permissions.size()]);
+            requestPermissions(params, REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+        } // else: We already have permissions, so handle as normal
+    }
+
+
 
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and firstName
