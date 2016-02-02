196fdf54cf201d7cc9582afe35c26f7581419ff3
ctruong <cminh.truong@gmail.com>
2016-02-02 15:35:27 +0100

    #Modify message sending from notification server US62

--- server-notification/src/main/java/fr/pds/isintheair/ctruong/uc/prospect/websocket/ProspectNotifEndPoint.java
+++ server-notification/src/main/java/fr/pds/isintheair/ctruong/uc/prospect/websocket/ProspectNotifEndPoint.java
@@ -4,8 +4,6 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -14,6 +12,7 @@ import java.util.concurrent.TimeUnit;
 /**
  * The EndPoint to connect to the websocket server
  * Created by Truong on 1/23/2016.
+ *
  * @version 1.0.0
  * @since 2016-01-24
  */
@@ -23,22 +22,20 @@ public class ProspectNotifEndPoint {
     static ScheduledExecutorService timer =
             Executors.newSingleThreadScheduledExecutor();
     private static Set<Session> allSession;
-    DateTimeFormatter timeFormatter =
-            DateTimeFormatter.ofPattern("HH:mm:ss");
+
 
     @OnOpen
-    public void onOpen(Session session){
+    public void onOpen(Session session) {
         allSession = session.getOpenSessions();
-        timer.scheduleAtFixedRate(() -> sendTimeToAll(session), 0, 1, TimeUnit.DAYS);
+        timer.scheduleAtFixedRate(() -> sendTimeToAll(session), 7, 1, TimeUnit.DAYS);
     }
 
-    private void sendTimeToAll(Session session){
+    private void sendTimeToAll(Session session) {
         allSession = session.getOpenSessions();
 
-        for (Session session1: allSession){
+        for (Session session1 : allSession) {
             try {
-                session1.getBasicRemote().sendText("Local time: " +
-                        LocalTime.now().format(timeFormatter));
+                session1.getBasicRemote().sendText("The analyse is finished. You've a new prospect. Let's see him");
 
             } catch (IOException e) {
                 e.printStackTrace();
