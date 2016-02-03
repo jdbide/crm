0607ca55006816446f02241c95e03da872d43737
ctruong <cminh.truong@gmail.com>
2016-02-03 21:45:40 +0100

    #Make change with Mockito US62

--- server-rest/src/test/java/api/ctruong/uc/prospect/suggest/Controller/ProspectControllerTest.java
+++ server-rest/src/test/java/api/ctruong/uc/prospect/suggest/Controller/ProspectControllerTest.java
@@ -7,25 +7,35 @@ import miage.pds.api.ctruong.uc.prospect.suggest.model.Sales;
 import miage.pds.api.ctruong.uc.prospect.suggest.model.User;
 import miage.pds.api.ctruong.uc.prospect.suggest.model.UserClientRelation;
 import miage.pds.api.ctruong.uc.prospect.suggest.service.MongoService;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.mongodb.morphia.Datastore;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
 
 /**
  * The unit test for the class prospect controller
- * <p>
+ * <p/>
  * Created by Truong on 12/20/2015.
  *
  * @version 1.1.19
  * @serial 111912202015
  */
+@RunWith(MockitoJUnitRunner.class)
+@PrepareForTest({ProspectController.class, Logger.class, ProspectDAOImpl.class, UserDAOImpl.class, UserClientRelationDAOImpl.class, SalesDAOImpl.class})
 public class ProspectControllerTest {
     private static final Logger log = LoggerFactory.getLogger(ProspectControllerTest.class);
     private MongoService service;
@@ -38,36 +48,44 @@ public class ProspectControllerTest {
     private MockTable mockTable;
 
 
-
     @Before
     public void setUp() throws Exception {
-        this.prospectController     = new ProspectController();
-        this.service                = new MongoService();
-        this.datastore              = service.getDatastore();
-        this.salesDAO               = new SalesDAOImpl(Sales.class, datastore);
-        this.userDAO                = new UserDAOImpl(User.class, datastore);
-        this.prospectDAO            = new ProspectDAOImpl(Prospect.class, datastore);
-        this.userClientRelationDAO  = new UserClientRelationDAOImpl(UserClientRelation.class, datastore);
-        this.mockTable              = new MockTable();
-        if (userDAO.count() > 0 && prospectDAO.count() > 0){
+        this.prospectController = new ProspectController();
+        this.service = new MongoService();
+        this.datastore = service.getDatastore();
+        this.salesDAO = new SalesDAOImpl(Sales.class, datastore);
+        this.userDAO = new UserDAOImpl(User.class, datastore);
+        this.prospectDAO = new ProspectDAOImpl(Prospect.class, datastore);
+        this.userClientRelationDAO = new UserClientRelationDAOImpl(UserClientRelation.class, datastore);
+        this.mockTable = new MockTable();
+        if (userDAO.count() > 0 && prospectDAO.count() > 0) {
             log.debug("Ok it's good, let begins all the test");
         } else {
             mockTable.mockClientTable();
             mockTable.mockRelationAndSalesTable();
             mockTable.mockUserTable();
         }
+        PowerMockito.mockStatic(ProspectController.class);
+        PowerMockito.mockStatic(Logger.class);
+        PowerMockito.mockStatic(ProspectDAOImpl.class);
+        PowerMockito.mockStatic(UserDAOImpl.class);
+        PowerMockito.mockStatic(UserClientRelationDAOImpl.class);
+        PowerMockito.mockStatic(SalesDAOImpl.class);
 
+        Logger logger = mock(Logger.class);
+        doNothing().when(logger).info(anyString());
+        PowerMockito.when(LoggerFactory.getLogger(anyString())).thenReturn(logger);
     }
 
-    public HashMap<User, ArrayList<Prospect>> init(){
+    public HashMap<User, ArrayList<Prospect>> init() {
 
-        HashMap<User, ArrayList<Prospect>>  hashMap     = new HashMap<User, ArrayList<Prospect>>();
-        List<User>                          list        = userDAO.getAllUsers();
-        for (User user: list){
-            List<Prospect> prospectListl= prospectDAO.getAllProspect();
-            ArrayList<Prospect>                 arrayList   = new ArrayList<Prospect>();
-            for (Prospect prospect: prospectListl){
-                if (!userClientRelationDAO.checkRelation(user.getId(), prospect.getId())){
+        HashMap<User, ArrayList<Prospect>> hashMap = new HashMap<User, ArrayList<Prospect>>();
+        List<User> list = userDAO.getAllUsers();
+        for (User user : list) {
+            List<Prospect> prospectListl = prospectDAO.getAllProspect();
+            ArrayList<Prospect> arrayList = new ArrayList<Prospect>();
+            for (Prospect prospect : prospectListl) {
+                if (!userClientRelationDAO.checkRelation(user.getId(), prospect.getId())) {
                     arrayList.add(prospect);
                 }
             }
@@ -94,13 +112,13 @@ public class ProspectControllerTest {
     @Test
     public void testGetSalesAverage() throws Exception {
         List<Sales> salesList = salesDAO.getAllSales();
-        int         count     = salesList.size();
-        double      sum       = 0.0d;
+        int count = salesList.size();
+        double sum = 0.0d;
         log.info("check sum: " + sum);
-        for (Sales sales : salesList){
+        for (Sales sales : salesList) {
             sum = sum + sales.getValue();
         }
-        double      average   = sum /count;
+        double average = sum / count;
         assertTrue(average == prospectController.getSalesAverage());
     }
 
@@ -130,7 +148,6 @@ public class ProspectControllerTest {
         HashMap<User, ArrayList<Prospect>> userMap = prospectController.analyzeProspectByPlaceNumber(hashMap);
         assertNotNull(userMap);
     }
-
 
 
 }
