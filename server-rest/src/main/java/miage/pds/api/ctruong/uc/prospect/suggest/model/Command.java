7e829d7b512a89c9215a4a2cff74c9a6b05c24bb
ctruong <cminh.truong@gmail.com>
2016-02-06 04:25:40 +0100

    #New command model US62

---
+++ server-rest/src/main/java/miage/pds/api/ctruong/uc/prospect/suggest/model/Command.java
@@ -0,0 +1,91 @@
+package miage.pds.api.ctruong.uc.prospect.suggest.model;
+
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.Property;
+
+import java.util.Date;
+
+/**
+ * Created by Truong on 2/6/2016.
+ */
+@Entity("command")
+public class Command {
+
+    @Id
+    private ObjectId id;
+
+    @Property
+    private long prospectId;
+
+    @Property
+    private String person;
+
+    @Property
+    private long value;
+
+    @Property
+    private Date date;
+
+    public Command() {
+    }
+
+    public Command(long prospectId, String person, long value, Date date) {
+        this.prospectId = prospectId;
+        this.person = person;
+        this.value = value;
+        this.date = date;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public long getProspectId() {
+        return prospectId;
+    }
+
+    public void setProspectId(long prospectId) {
+        this.prospectId = prospectId;
+    }
+
+    public String getPerson() {
+        return person;
+    }
+
+    public void setPerson(String person) {
+        this.person = person;
+    }
+
+    public long getValue() {
+        return value;
+    }
+
+    public void setValue(long value) {
+        this.value = value;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Command{" +
+                "id=" + id +
+                ", prospectId=" + prospectId +
+                ", person='" + person + '\'' +
+                ", value=" + value +
+                ", date=" + date +
+                '}';
+    }
+}
