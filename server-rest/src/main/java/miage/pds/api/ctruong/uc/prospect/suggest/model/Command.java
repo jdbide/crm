4a55c7f19d2f65c7c9e7799c01c138ff172bf699
ctruong <cminh.truong@gmail.com>
2016-02-06 07:09:04 +0100

    #Change command model US62

--- server-rest/src/main/java/miage/pds/api/ctruong/uc/prospect/suggest/model/Command.java
+++ server-rest/src/main/java/miage/pds/api/ctruong/uc/prospect/suggest/model/Command.java
@@ -5,8 +5,6 @@ import org.mongodb.morphia.annotations.Entity;
 import org.mongodb.morphia.annotations.Id;
 import org.mongodb.morphia.annotations.Property;
 
-import java.util.Date;
-
 /**
  * Created by Truong on 2/6/2016.
  */
@@ -25,17 +23,13 @@ public class Command {
     @Property
     private long value;
 
-    @Property
-    private Date date;
-
     public Command() {
     }
 
-    public Command(ObjectId prospectId, String person, long value, Date date) {
+    public Command(ObjectId prospectId, String person, long value) {
         this.prospectId = prospectId;
         this.person = person;
         this.value = value;
-        this.date = date;
     }
 
     public ObjectId getId() {
@@ -70,22 +64,13 @@ public class Command {
         this.value = value;
     }
 
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
     @Override
     public String toString() {
         return "Command{" +
                 "id=" + id +
-                ", prospectId='" + prospectId + '\'' +
+                ", prospectId=" + prospectId +
                 ", person='" + person + '\'' +
                 ", value=" + value +
-                ", date=" + date +
                 '}';
     }
 }
