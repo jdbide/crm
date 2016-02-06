17ad91dc2ed60537e6850cf52c40f9f39a983f58
ctruong <cminh.truong@gmail.com>
2016-02-06 15:17:14 +0100

    #Change config US62

--- server-rest/src/main/java/miage/pds/api/ctruong/uc/prospect/suggest/controller/ProspectController.java
+++ server-rest/src/main/java/miage/pds/api/ctruong/uc/prospect/suggest/controller/ProspectController.java
@@ -1,8 +1,13 @@
 package miage.pds.api.ctruong.uc.prospect.suggest.controller;
 
+import miage.pds.api.ctruong.uc.prospect.suggest.dao.ProspectDAO;
+import miage.pds.api.ctruong.uc.prospect.suggest.dao.ProspectDAOImp;
+import miage.pds.api.ctruong.uc.prospect.suggest.model.Prospect;
 import miage.pds.api.ctruong.uc.prospect.suggest.service.MongoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 /**
  * This is a class which control the algorithm to find the prospect for each user.
@@ -15,6 +20,7 @@ public class ProspectController {
 
     private MongoService            mongoService;
     private static Logger logger = LoggerFactory.getLogger(ProspectController.class);
+    private ProspectDAO prospectDAO;
 
 
     /**
@@ -22,7 +28,13 @@ public class ProspectController {
      */
     public ProspectController() {
         this.mongoService           = new MongoService();
+        this.prospectDAO = new ProspectDAOImp(Prospect.class, mongoService.getDatastore());
     }
 
+    public List<Prospect> getListProspectByTurnover(){
+        List<Prospect> prospects = prospectDAO.getListProspect();
+
+        return prospects;
+    }
 
 }
