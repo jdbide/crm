1f553466cb1f1ef8f322acea969c178e3185aba5
mbalabascarin <muthukumarasamy.balabascarin@etu.u-pec.fr>
2016-02-06 18:38:33 +0100

    added mock visit class and renamed diagram folder

---
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/mbalabascarin/uc/edit/crv/mock/MockVisit.java
@@ -0,0 +1,7 @@
+package fr.pds.isintheair.crmtab.mbalabascarin.uc.edit.crv.mock;
+
+/**
+ * Created by Muthu on 06/02/2016.
+ */
+public class MockVisit {
+}
--- android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/mbalabascarin/uc/edit/crv/view/VisitActivity.java
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/mbalabascarin/uc/edit/crv/view/VisitActivity.java
@@ -1,16 +1,21 @@
 package fr.pds.isintheair.crmtab.mbalabascarin.uc.edit.crv.view;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import fr.pds.isintheair.crmtab.R;
 
-public class VisitActivity extends Activity {
+public class VisitActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visit);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
+
 
 }
