b91c4c679dec9d50183a6869b44e48e29ba6911b
jdatour <julien.datour@gmail.com>
2016-02-06 13:56:56 +0100

    New URL for call notifier, updated for phonintheair and crmtab

--- android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/jdatour/uc/phone/call/receive/model/constant/Constant.java
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/jdatour/uc/phone/call/receive/model/constant/Constant.java
@@ -1,6 +1,6 @@
 package fr.pds.isintheair.crmtab.jdatour.uc.phone.call.receive.model.constant;
 
 public class Constant {
-    public static String WS_URL = "ws://192.168.20.3:8090";
+    public static String WS_URL = "ws://192.168.20.3:8090/call";
     //public static String WS_URL = "ws://192.168.1.1:8090";
 }
