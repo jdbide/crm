874372863a0b61cbaee37724b3be754a3d318548
ctruong <cminh.truong@gmail.com>
2016-02-06 05:03:29 +0100

    #Update ProspectDAO US62

--- server-rest/src/main/java/miage/pds/api/ctruong/uc/prospect/suggest/dao/ProspectDAOImp.java
+++ server-rest/src/main/java/miage/pds/api/ctruong/uc/prospect/suggest/dao/ProspectDAOImp.java
@@ -6,14 +6,26 @@ import org.bson.types.ObjectId;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.dao.BasicDAO;
 
+import java.util.List;
+
 /**
  * Created by Truong on 2/6/2016.
  */
-public class ProspectDAOImp extends BasicDAO<Prospect, ObjectId>{
+public class ProspectDAOImp extends BasicDAO<Prospect, ObjectId> implements ProspectDAO{
 
     private Datastore datastore;
 
     public ProspectDAOImp(Class<Prospect> entityClass, Datastore ds) {
         super(entityClass, ds);
+    }
+
+    @Override
+    public Prospect getProspectById(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Prospect> getListProspect() {
+        return null;
     }
 }
