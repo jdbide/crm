46af502fd24bcafbd77b753110f45de7c9e42451
jdatour <julien.datour@gmail.com>
2016-02-07 21:05:39 +0100

    Multiple environments are now handled for crmtab, phonintheair and notifier

--- server-notification/src/main/java/fr/pds/isintheair/jdatour/uc/phone/call/receive/server/NotifierServer.java
+++ server-notification/src/main/java/fr/pds/isintheair/jdatour/uc/phone/call/receive/server/NotifierServer.java
@@ -2,14 +2,43 @@ package fr.pds.isintheair.jdatour.uc.phone.call.receive.server;
 
 import org.glassfish.tyrus.server.Server;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.util.Properties;
+
 public class NotifierServer {
 
     public static void main(String[] args) {
-        runServer();
+        try {
+            runServer();
+        }
+        catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void runServer() {
-        Server server = new Server("localhost", 8090, "/", ServerConfiguration.class);
+    public static void runServer() throws URISyntaxException, IOException {
+        String port = null;
+        Properties properties = new Properties();
+        InputStream inputStream = null;
+
+        try {
+            inputStream = NotifierServer.class.getClassLoader().getResourceAsStream("configuration.properties");
+
+            properties.load(inputStream);
+
+            port = properties.getProperty("port");
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+
+        Server server = new Server("localhost", Integer.parseInt(port), "/", ServerConfiguration.class);
 
         ((Runnable) () -> {
             try {
