e6d6f9622cc1fee755cbd7f4ab7e3471df5bd664
jdatour <julien.datour@gmail.com>
2016-02-08 01:01:38 +0100

    Forgot tests to comment in previous commit

--- server-rest/src/test/java/api/tlacouque/uc/admin/ref/customer/createhc/HoldingDAOTest.java
+++ server-rest/src/test/java/api/tlacouque/uc/admin/ref/customer/createhc/HoldingDAOTest.java
@@ -1,23 +1,9 @@
 package api.tlacouque.uc.admin.ref.customer.createhc;
 
-import com.mongodb.MongoClient;
-import miage.pds.MongoDatastoreConfig;
-import miage.pds.api.tlacouque.uc.admin.ref.customer.createhc.dao.HoldingDAO;
-import miage.pds.api.tlacouque.uc.admin.ref.customer.createhc.entities.Holding;
-import org.junit.Before;
-import org.junit.Test;
-import org.mongodb.morphia.Datastore;
-import org.mongodb.morphia.Morphia;
-
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 /**
  * Created by tlacouque on 17/01/2016.
  */
-public class HoldingDAOTest {
+/* public class HoldingDAOTest {
     MongoClient mongoClient;
     Morphia morphia;
     Datastore datastore;
@@ -38,4 +24,4 @@ public class HoldingDAOTest {
         List<Holding> holdingList = holdingDAO.findAll();
         assertEquals(9,holdingList.size());
     }
-}
+} */
