07ce080cc0409c9902b58dd0e87efb23d300eb19
tlacouque <titouanlac77@gmail.com>
2016-02-27 16:27:18 +0100

    #ALL PROJECT : Update package name for constant

--- android-crmtab/app/src/prod/java/fr/pds/isintheair/crmtab/jdatour/uc/phone/call/receive/model/constant/Constant.java
+++ android-crmtab/app/src/prod/java/fr/pds/isintheair/crmtab/jdatour/uc/phone/call/receive/model/constant/Constant.java
@@ -1,4 +1,4 @@
-package crmtab.jdatour.uc.phone.call.receive.model.constant;
+package fr.pds.isintheair.crmtab.jdatour.uc.phone.call.receive.model.constant;
 
 public class Constant {
     public static String WS_URL = "ws://192.168.20.3:8083/call";
