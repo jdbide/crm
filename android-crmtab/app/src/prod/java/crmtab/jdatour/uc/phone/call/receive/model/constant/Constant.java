7496a7dc44d12cacbaa2ea8082384edaca08648f
tlacouque <titouanlac77@gmail.com>
2016-02-27 16:16:49 +0100

    #ALL PROJECT : Update package for jdatour constant

--- android-crmtab/app/src/prod/java/crmtab/jdatour/uc/phone/call/receive/model/constant/Constant.java
+++ android-crmtab/app/src/prod/java/crmtab/jdatour/uc/phone/call/receive/model/constant/Constant.java
@@ -1,4 +1,4 @@
-package fr.pds.isintheair.crmtab.jdatour.uc.phone.call.receive.model.constant;
+package crmtab.jdatour.uc.phone.call.receive.model.constant;
 
 public class Constant {
     public static String WS_URL = "ws://192.168.20.3:8083/call";
