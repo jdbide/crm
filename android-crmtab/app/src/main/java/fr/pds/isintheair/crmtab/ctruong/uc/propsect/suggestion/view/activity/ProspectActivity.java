2c059a068fe126d02b2b5b2961eec8547efeb872
ctruong <cminh.truong@gmail.com>
2016-03-01 22:09:26 +0100

    Modified in Prospect Activity

--- android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/ctruong/uc/propsect/suggestion/view/activity/ProspectActivity.java
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/ctruong/uc/propsect/suggestion/view/activity/ProspectActivity.java
@@ -1,18 +1,19 @@
 package fr.pds.isintheair.crmtab.ctruong.uc.propsect.suggestion.view.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ListView;
 
-import com.squareup.okhttp.ResponseBody;
-
-import java.io.IOException;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import fr.pds.isintheair.crmtab.R;
+import fr.pds.isintheair.crmtab.ctruong.uc.propsect.suggestion.adapter.ProspectAdapter;
 import fr.pds.isintheair.crmtab.ctruong.uc.propsect.suggestion.model.config.ProspectRestConfig;
 import fr.pds.isintheair.crmtab.ctruong.uc.propsect.suggestion.model.domain.Prospect;
 import fr.pds.isintheair.crmtab.ctruong.uc.propsect.suggestion.rest.ProspectRetrofitAPI;
@@ -26,8 +27,11 @@ public class ProspectActivity extends Activity {
 
     @Bind(R.id.btn_prospect)
     Button btn_prospect;
-    @Bind(R.id.tv_prospect)
-    TextView tv_prospect;
+    @Bind(R.id.lview_prospect)
+    ListView lview_prospect;
+    List<Prospect> prospects;
+    ProspectAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,31 +43,26 @@ public class ProspectActivity extends Activity {
                 Retrofit retrofit = new Retrofit.Builder().baseUrl(ProspectRestConfig.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
                 ProspectRetrofitAPI api = retrofit.create(ProspectRetrofitAPI.class);
                 Call call = api.getProspect();
-                call.enqueue(new Callback<Prospect>() {
-                    @Override
-                    public void onResponse(Response<Prospect> response, Retrofit retrofit) {
-                        Prospect prospect = response.body();
+                call.enqueue(new Callback<List<Prospect>>() {
 
-                        if (prospect == null){
-                            // 404 error or others
-                            ResponseBody body = response.errorBody();
-                            if (body!= null){
-                                try {
-                                    tv_prospect.setText("response body: " + body.string());
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                }
-                            } else {
-                                tv_prospect.setText("response body = null");
+                    @Override
+                    public void onResponse(Response<List<Prospect>> response, Retrofit retrofit) {
+
+                        adapter = new ProspectAdapter(ProspectActivity.this, 0, prospects);
+                        lview_prospect.setAdapter(adapter);
+                        lview_prospect.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                Intent intent = new Intent(getApplicationContext(), ConsultProspect.class);
+                                intent.putExtra("prospect", position);
+                                startActivityForResult(intent, 0);
                             }
-                        } else {
-                            tv_prospect.setText("Name : " + prospect.getName() + "\nSiret Number : " + prospect.getSiretNumber());
-                        }
+                        });
                     }
 
                     @Override
                     public void onFailure(Throwable t) {
-                        tv_prospect.setText("t = " + t.getMessage());
+
                     }
                 });
             }
