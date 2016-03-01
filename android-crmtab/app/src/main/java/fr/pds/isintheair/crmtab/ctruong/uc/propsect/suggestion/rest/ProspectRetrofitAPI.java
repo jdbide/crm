5dc33b41b9a34f23377307da54ac159739521713
ctruong <cminh.truong@gmail.com>
2016-03-01 20:33:39 +0100

    Create POST request

--- android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/ctruong/uc/propsect/suggestion/rest/ProspectRetrofitAPI.java
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/ctruong/uc/propsect/suggestion/rest/ProspectRetrofitAPI.java
@@ -3,6 +3,8 @@ package fr.pds.isintheair.crmtab.ctruong.uc.propsect.suggestion.rest;
 import fr.pds.isintheair.crmtab.ctruong.uc.propsect.suggestion.model.domain.Prospect;
 import retrofit.Call;
 import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.Path;
 
 /**
  * Created by Truong on 2/10/2016.
@@ -11,4 +13,7 @@ public interface ProspectRetrofitAPI {
 
     @GET("suggestion/prospect")
     Call<Prospect> getProspect();
+
+    @POST("suggestion/prospect/{siret}")
+    Call<Prospect> insertProspect(@Path("siret") long siret);
 }
