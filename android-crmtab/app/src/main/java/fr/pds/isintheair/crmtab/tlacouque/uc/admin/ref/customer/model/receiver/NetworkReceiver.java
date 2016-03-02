db6e165004f766557ce3233d91ac22184d0ace80
tlacouque <titouanlac77@gmail.com>
2016-03-02 21:40:37 +0100

    #ONLINE MAP : Added broadcast receiver

---
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/tlacouque/uc/admin/ref/customer/model/receiver/NetworkReceiver.java
@@ -0,0 +1,16 @@
+package fr.pds.isintheair.crmtab.tlacouque.uc.admin.ref.customer.model.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by tlacouque on 02/03/2016.
+ */
+public class NetworkReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
