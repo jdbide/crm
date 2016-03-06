fcfbd98e47ddf8276a8b2df5707328d00c23d2e9
tlacouque <titouanlac77@gmail.com>
2016-03-06 17:36:58 +0100

    #ALL Project : Update constant value for prod. Added rest constant for muthu US

--- android-crmtab/app/src/prod/java/fr/pds/isintheair/crmtab/jdatour/uc/phone/call/receive/model/constant/Constant.java
+++ android-crmtab/app/src/prod/java/fr/pds/isintheair/crmtab/jdatour/uc/phone/call/receive/model/constant/Constant.java
@@ -2,5 +2,10 @@ package fr.pds.isintheair.crmtab.jdatour.uc.phone.call.receive.model.constant;
 
 public class Constant {
     public static String WS_URL = "ws://192.168.20.3:8083/call";
-    public static String REST_URL = "http://192.168.20.3:8081";
+
+    // url for rest server
+    public static final String REST_URL = "http://192.168.20.3:8081";
+
+    //url for mock client
+    public static final String BASE_URL = "http://192.168.20.3:8081/api/";
 }
