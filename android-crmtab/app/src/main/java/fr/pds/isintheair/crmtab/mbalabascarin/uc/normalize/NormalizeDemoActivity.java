a28cc6dbd7d60201d1c1978ca4e95cbee0af5f4b
Muthu Balabascarin <muthukumarasamy.balabascarin@etu.u-pec.fr>
2016-03-11 23:51:24 +0100

    #normalize-app: added pattern for mail normalization

--- android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/mbalabascarin/uc/normalize/NormalizeDemoActivity.java
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/mbalabascarin/uc/normalize/NormalizeDemoActivity.java
@@ -243,6 +243,15 @@ public class NormalizeDemoActivity extends Activity implements SpellCheckerSessi
         return false;
     }
 
+    public static final Pattern EMAIL_ADDRESS_PATTERN = Pattern.compile(
+            "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                    "\\@" +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                    "(" +
+                    "\\." +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                    ")+"
+    );
 
 
 
