5a2cfd299bf94b11d663263a2c46e4091c68975f
ctruong <cminh.truong@gmail.com>
2016-03-13 10:26:37 +0100

    Stop using local url

--- android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/ctruong/uc/propsect/suggestion/model/config/ProspectRestConfig.java
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/ctruong/uc/propsect/suggestion/model/config/ProspectRestConfig.java
@@ -5,4 +5,5 @@ package fr.pds.isintheair.crmtab.ctruong.uc.propsect.suggestion.model.config;
  */
 public interface ProspectRestConfig {
     static final String BASE_URL = "http://192.168.20.3:8082/api/";
+//    static final String BASE_URL = "http://192.168.0.11:8080/";
 }
