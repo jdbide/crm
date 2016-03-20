bb984be997349a2536849d44ae69d33269644634
ctruong <cminh.truong@gmail.com>
2016-03-20 12:13:55 +0100

    #Prospect Suggestion:   Create new client endpoint

--- server-rest/src/main/java/miage/pds/api/ctruong/uc/prospect/suggest/ws/ProspectClientWS.java
+++ server-rest/src/main/java/miage/pds/api/ctruong/uc/prospect/suggest/ws/ProspectClientWS.java
@@ -1,7 +1,11 @@
 package miage.pds.api.ctruong.uc.prospect.suggest.ws;
 
+import javax.websocket.ClientEndpoint;
+
 /**
  * Created by Truong on 3/20/2016.
  */
+@ClientEndpoint
 public class ProspectClientWS {
+
 }
