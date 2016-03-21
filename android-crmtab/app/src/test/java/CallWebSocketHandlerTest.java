39e231db200913b93d40798f0b23ab4e65ad4d72
jdatour <julien.datour@gmail.com>
2016-03-21 19:23:55 +0100

    Refactored test packages

---
+++ android-crmtab/app/src/test/java/CallWebSocketHandlerTest.java
@@ -0,0 +1,20 @@
+/******************************************
+ * Created by        : jdatour            *
+ * Creation date     : 01/24/16           *
+ * Modified by       :                    *
+ * Modification date :                    *
+ ******************************************/
+
+/* public class CallWebSocketHandlerTest {
+    @Test
+    public void testRegisterCalledOnOpen() {
+        WebSocketConnectionHandlerSingleton webSocketConnectionHandlerSingleton = Mockito.spy(WebSocketConnectionHandlerSingleton.getInstance());
+
+        MessageMeta messageMeta = new MessageMeta.MessageMetaBuilder().addMessageType(MessageType.REGISTER_PHONE).build();
+        Register    register    = new Register(42);
+        Message     message     = new Message.MessageBuilder().addMessageInfo(messageMeta).addRegister(register).build();
+
+        WebSocketConnectionHandlerSingleton.getInstance().connectToCall();
+        Mockito.verify(webSocketConnectionHandlerSingleton, Mockito.times(1)).sendMessage(message);
+    }
+} */
