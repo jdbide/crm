4916ce23126fbc1aa7cba84b1e1ae5d1629aa996
ctruong <cminh.truong@gmail.com>
2016-03-22 21:18:46 +0100

    #Prospect suggestion:   Try to create 2 unit tests in Android

---
+++ android-crmtab/app/src/test/java/fr/pds/isintheair/crmtab/DetailProspectTest.java
@@ -0,0 +1,11 @@
+package fr.pds.isintheair.crmtab;
+
+import fr.pds.isintheair.crmtab.ctruong.uc.propsect.suggestion.view.activity.DetailProspect;
+
+/**
+ * Created by Truong on 3/22/2016.
+ */
+
+//public class DetailProspectTest {
+//    DetailProspect detailProspect;
+//}
