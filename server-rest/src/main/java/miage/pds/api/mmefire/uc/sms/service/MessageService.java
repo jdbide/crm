3873c3b92dbc3e703e72c84da47ffeb1f886c469
jdatour <julien.datour@gmail.com>
2016-03-23 01:19:12 +0100

    ...

--- server-rest/src/main/java/miage/pds/api/mmefire/uc/sms/service/MessageService.java
+++ server-rest/src/main/java/miage/pds/api/mmefire/uc/sms/service/MessageService.java
@@ -1,60 +1,60 @@
 package miage.pds.api.mmefire.uc.sms.service;
 
+import miage.pds.api.mmefire.uc.sms.dao.Message;
+import miage.pds.api.mmefire.uc.sms.dao.MessageDao;
+import miage.pds.api.mmefire.uc.sms.model.MessageDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
-import miage.pds.api.mmefire.uc.sms.dao.Message;
-import miage.pds.api.mmefire.uc.sms.dao.MessageDao;
-import miage.pds.api.mmefire.uc.sms.model.MessageDTO;
-
 public class MessageService {
 
-	private MessageDao messageDAO;
+    private MessageDao messageDAO;
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(MessageService.class.getName());
+    private static final Logger logger = LoggerFactory.getLogger(MessageService.class.getName());
 
-	private static MessageService instance;
+    private static MessageService instance;
 
-	/**
-	 * 
-	 */
-	private MessageService() {
-		logger.debug("Cr�ation du singleton MessageService");
-		messageDAO = MessageDao.getIntance();
-	}
+    /**
+     *
+     */
+    private MessageService () {
+        logger.debug("Cr�ation du singleton MessageService");
+        //  messageDAO = MessageDao.getIntance();
+    }
 
-	/**
-	 * Retourne l'instance unique pour ne plus creer plusieurs objet
-	 * 
-	 * @return
-	 */
-	public static MessageService getInstance() {
-		if (instance == null) {
-			instance = new MessageService();
-		}
-		return instance;
-	}
-// creer un message en BD
-	private void addMessage(MessageDTO messageDto) {
+    /**
+     * Retourne l'instance unique pour ne plus creer plusieurs objet
+     *
+     * @return
+     */
+    public static MessageService getInstance () {
+        if (instance == null) {
+            instance = new MessageService();
+        }
+        return instance;
+    }
 
-		Message message = new Message(messageDto.content, messageDto.sender,
-				messageDto.recipient, messageDto.date);
+    // creer un message en BD
+    public void addMessage (MessageDTO messageDto) {
+
+       /*  Message message = new Message(messageDto.content, messageDto.sender, messageDto.recipient, messageDto.date);
 
 
-		messageDAO.addMessage(message);
+        messageDAO.addMessage(message); */
 
-	}
-// permet de recuperer un liste de message � partir de l'id du contact passe en parametre
-	private List<Message> getMessagesForContact(String contactID){
+    }
 
-		return messageDAO.getAllMessageForContact(contactID);
+    // permet de recuperer un liste de message � partir de l'id du contact passe en parametre
+    public List<Message> getMessagesForContact (String contactID) {
 
-	}
- // suuprimer un message � partir de son identifiant
-	private boolean deleteMessageById(String messageID){
-		return messageDAO.deleteMesageById(messageID);
-	}
+        return messageDAO.getAllMessageForContact(contactID);
+
+    }
+
+    // suuprimer un message � partir de son identifiant
+    private boolean deleteMessageById (String messageID) {
+        return messageDAO.deleteMesageById(messageID);
+    }
 }
