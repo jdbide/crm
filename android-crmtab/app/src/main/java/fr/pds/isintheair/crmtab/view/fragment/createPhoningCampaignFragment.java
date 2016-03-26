bb4acacd2d32e8053cb3e3354750b6ebc56b22aa
tlacouque <titouanlac77@gmail.com>
2016-03-26 22:15:40 +0100

    #US create phoning campaign : added fragment

---
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/view/fragment/createPhoningCampaignFragment.java
@@ -0,0 +1,10 @@
+package fr.pds.isintheair.crmtab.view.fragment;
+
+import android.app.Fragment;
+
+/**
+ * Created by tlacouque on 26/03/2016.
+ */
+public class createPhoningCampaignFragment extends Fragment {
+
+}
