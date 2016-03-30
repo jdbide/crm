6b57895312542cc5cf6a78cd37e3a22a125d8dd2
jbide <bide26@gmail.com>
2016-03-30 13:28:32 +0200

    added url

--- android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/controller/NotifyPresenceInterface.java
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/controller/NotifyPresenceInterface.java
@@ -1,6 +1,7 @@
 package fr.pds.isintheair.crmtab.controller;
 
 import fr.pds.isintheair.crmtab.model.ClockinObject;
+import fr.pds.isintheair.crmtab.model.entity.Tag;
 import retrofit.Call;
 import retrofit.http.Body;
 import retrofit.http.POST;
@@ -12,5 +13,8 @@ public interface NotifyPresenceInterface {
 
     @POST("notifypresence/clockin")
     Call<ClockinObject> clockin(@Body ClockinObject clockinObject);
+
+    @POST("notifypresence/addnewtag")
+    Call<Boolean> addNewTag(@Body Tag tag);
 
 }
