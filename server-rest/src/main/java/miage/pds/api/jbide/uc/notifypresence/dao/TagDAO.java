8a8b75525e4475336a0dc51c646d66353decd453
jbide <bide26@gmail.com>
2016-03-30 12:18:35 +0200

    updated TagDao class on server

--- server-rest/src/main/java/miage/pds/api/jbide/uc/notifypresence/dao/TagDAO.java
+++ server-rest/src/main/java/miage/pds/api/jbide/uc/notifypresence/dao/TagDAO.java
@@ -6,9 +6,14 @@ import org.mongodb.morphia.dao.BasicDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import miage.pds.api.common.model.ClockinObject;
 import miage.pds.api.jbide.uc.notifypresence.model.Tag;
 import miage.pds.api.jbide.uc.registercall.dao.CraDAO;
 import miage.pds.api.jbide.uc.registercall.model.Cra;
+
+/**
+ * Created by jbide on 30/03/2016.
+ */
 
 public class TagDAO  extends BasicDAO<Cra, ObjectId> {
 	
@@ -20,15 +25,18 @@ public class TagDAO  extends BasicDAO<Cra, ObjectId> {
 
 	// Tag insertion
 	public boolean addTag(Tag tag) {
-		boolean state = false;
-		// check if connected to DB
-		//cra.setIdcra(getUniqueIdCra());
-		//getDatastore().save(cra);
+		getDatastore().save(tag);
 		return true;
 	}
 	
+	//
+	public boolean updateLocation(ClockinObject object) {
+		  getDatastore().createQuery(Tag.class).disableValidation().field("id").equal(tag.getId()).get();
+		  return true;
+	}
+	
 	public boolean deleteTag(String idTag) {
-		//getDatastore().delete(getDatastore().createQuery(Cra.class).field("idcra").equal(idcra));
+		getDatastore().delete(getDatastore().createQuery(Tag.class).field("id").equal(idTag));
 		return true;
 	}
 
