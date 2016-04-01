0ca33d840e781ef0ba146547f7b2960722c0a63d
jbide <bide26@gmail.com>
2016-04-01 17:29:12 +0200

    now clockin in from mainactivity

---
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/controller/message/ClockinController.java
@@ -0,0 +1,7 @@
+package fr.pds.isintheair.crmtab.controller.message;
+
+/**
+ * Created by jbide on 01/04/2016.
+ */
+public class ClockinController {
+}
--- android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/view/activity/MainActivity.java
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/view/activity/MainActivity.java
@@ -3,23 +3,36 @@ package fr.pds.isintheair.crmtab.view.activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.NfcF;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.otto.Subscribe;
 
+import java.util.Arrays;
+
 import fr.pds.isintheair.crmtab.R;
 import fr.pds.isintheair.crmtab.controller.bus.BusHandlerSingleton;
+import fr.pds.isintheair.crmtab.controller.message.ClockinController;
 import fr.pds.isintheair.crmtab.controller.message.CrvController;
 import fr.pds.isintheair.crmtab.controller.service.CalendarService;
 import fr.pds.isintheair.crmtab.controller.service.CallService;
@@ -57,11 +70,18 @@ public class MainActivity extends AppCompatActivity
     User    currentUser;
     // UC Register a call
     private PendingLogsFragment pend;
-    private BusHandlerSingleton bus;
+    private TextView mTextView;
+    private NfcAdapter mNfcAdapter;
+    private PendingIntent mPendingIntent;
+    private IntentFilter[] mIntentFilters;
+    private String[][] mNFCTechLists;
+    private ImageView imgStatus;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar1);
         setSupportActionBar(toolbar);
@@ -89,7 +109,96 @@ public class MainActivity extends AppCompatActivity
 
         if (getIntent().hasExtra("msg"))
             showNotificationListFrag();
+
+        //Badging
+        // mTextView = (TextView)findViewById(R.id.tag);
+        imgStatus = (ImageView)findViewById(R.id.imgStatus);
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (mNfcAdapter != null) {
+            // mTextView.setText("Read an NFC tag");
+        } else {
+            //mTextView.setText("This phone is not NFC enabled.");
+        }
+
+        // create an intent with tag data and deliver to this activity
+        mPendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        // set an intent filter for all MIME data
+        IntentFilter ndefIntent = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        try {
+            ndefIntent.addDataType("*/*");
+            mIntentFilters = new IntentFilter[] { ndefIntent };
+        } catch (Exception e) {
+            Log.e("TagDispatch", e.toString());
+        }
+
+        mNFCTechLists = new String[][] { new String[] { NfcF.class.getName() } };
     }
+
+
+    //Badging
+    @Override
+    public void onNewIntent(Intent intent) {
+        String action = intent.getAction();
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+        //String s = action + "\n\n" + tag.toString();
+        String s="";
+
+        // parse through all NDEF messages and their records and pick text type only
+        Parcelable[] data = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+
+        if (data != null) {
+            try {
+                for (int i = 0; i < data.length; i++) {
+                    NdefRecord[] recs = ((NdefMessage)data[i]).getRecords();
+                    for (int j = 0; j < recs.length; j++) {
+                        if (recs[j].getTnf() == NdefRecord.TNF_WELL_KNOWN &&
+                                Arrays.equals(recs[j].getType(), NdefRecord.RTD_TEXT)) {
+
+                            byte[] payload = recs[j].getPayload();
+                            String textEncoding = ((payload[0] & 0200) == 0) ? "UTF-8" : "UTF-16";
+                            int langCodeLen = payload[0] & 0077;
+
+                            s = ( new String(payload, langCodeLen + 1,
+                                    payload.length - langCodeLen - 1, textEncoding));
+
+
+                            ClockinController.clockin(s,this);
+
+
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                Log.e("TagDispatch", e.toString());
+            }
+
+        }
+
+        // mTextView.setText(s);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (mNfcAdapter != null)
+            mNfcAdapter.enableForegroundDispatch(this, mPendingIntent, mIntentFilters, mNFCTechLists);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (mNfcAdapter != null)
+            mNfcAdapter.disableForegroundDispatch(this);
+    }
+
+
+
 
     /**
      * @Override public void onBackPressed() {
@@ -168,14 +277,7 @@ public class MainActivity extends AppCompatActivity
             transaction.commit();*/
 
         }
-        else if(id == R.id.nfc){
 
-                final Intent intent = new Intent(NfcAdapter.ACTION_NDEF_DISCOVERED);
-                //intent.setType("text/plain");
-                intent.putExtra(NfcAdapter.EXTRA_NDEF_MESSAGES, "tagid");
-                startActivity(intent);
-
-        }
         else if (id == R.id.nav_ref_client) {
             // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar1);
             // setSupportActionBar(toolbar);
