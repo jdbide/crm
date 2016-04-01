4627cdae70c4bbf1aa6c894f6d6b89ba9652491c
jbide <bide26@gmail.com>
2016-04-01 14:50:43 +0200

    renamed retrofit interface

--- android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/model/rest/service/NotifyPresenceRetrofitService.java
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/model/rest/service/NotifyPresenceRetrofitService.java
@@ -1,4 +1,4 @@
-package fr.pds.isintheair.crmtab.controller;
+package fr.pds.isintheair.crmtab.model.rest.service;
 
 import fr.pds.isintheair.crmtab.model.ClockinObject;
 import fr.pds.isintheair.crmtab.model.entity.Tag;
@@ -9,7 +9,7 @@ import retrofit.http.POST;
 /**
  * Created by jbide on 29/03/2016.
  */
-public interface NotifyPresenceInterface {
+public interface NotifyPresenceRetrofitService {
 
     @POST("notifypresence/clockin")
     Call<ClockinObject> clockin(@Body ClockinObject clockinObject);
