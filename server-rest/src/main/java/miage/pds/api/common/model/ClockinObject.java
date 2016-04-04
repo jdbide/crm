c26bf27480323d04e535ffe44503167118d360ce
jbide <bide26@gmail.com>
2016-04-04 15:41:33 +0200

    #US111 #TA571 updated rest object

--- server-rest/src/main/java/miage/pds/api/common/model/ClockinObject.java
+++ server-rest/src/main/java/miage/pds/api/common/model/ClockinObject.java
@@ -1,13 +1,31 @@
 package miage.pds.api.common.model;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class ClockinObject {
 
+
     private User user;
     private String tagId;
-    private Date date;
-    
+    private String date;
+    private String time;
+
+    public ClockinObject() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public ClockinObject(User user, String tagId) {
+        this.user = user;
+        this.tagId = tagId;
+        Calendar c = Calendar.getInstance(Locale.FRANCE);
+        date = c.get(c.DAY_OF_MONTH)+"-"+c.get(c.MONTH)+"-"+c.get(c.YEAR);
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        time = sdf.format(new Date());
+    }
+
     public User getUser() {
         return user;
     }
@@ -23,10 +41,20 @@ public class ClockinObject {
     public void setTagId(String tagId) {
         this.tagId = tagId;
     }
-    
-    public Date getDate() {
+
+    public String getDate() {
         return date;
     }
 
+    public void setDate(String date) {
+        this.date = date;
+    }
 
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 }
