4d76369a723befdf826313c7ad9c43c82ed9c9f9
tlacouque <titouanlac77@gmail.com>
2016-04-07 20:05:43 +0200

    #US Begin Phoning Campaign : Added comments

--- android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/helper/PhoningCampaignHelper.java
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/helper/PhoningCampaignHelper.java
@@ -8,10 +8,18 @@ import fr.pds.isintheair.crmtab.model.entity.Customer;
 
 /**
  * Created by tlacouque on 03/04/2016.
+ * Class used to help the phoning campaign controller
  */
 public class PhoningCampaignHelper {
 
-
+    /**
+     * Static method used to know if the currentContact pass by the position is the last contact
+     * of a customer pass in parameter in the linked list in parameter too.
+     * @param customerListHashMap
+     * @param currentContactPosition
+     * @param currentCustomer
+     * @return
+     */
     public static boolean isLastContact(LinkedHashMap<Customer,List<Contact>> customerListHashMap,
                                         int currentContactPosition,Customer currentCustomer) {
         int futurContactPosition = currentContactPosition + 1;
