7e8f5cdc16065fc31cda321d7f40725f48488aa2
ctruong <cminh.truong@gmail.com>
2016-04-24 14:29:17 +0200

    #Synchro UC  - Add new event

--- android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/ctruong/uc/propsect/suggestion/view/activity/SynchronisationActivity.java
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/ctruong/uc/propsect/suggestion/view/activity/SynchronisationActivity.java
@@ -2,14 +2,20 @@ package fr.pds.isintheair.crmtab.ctruong.uc.propsect.suggestion.view.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import fr.pds.isintheair.crmtab.R;
 
-public class SynchronisationActivity extends Activity {
+public class SynchronisationActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_synchronisation);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
