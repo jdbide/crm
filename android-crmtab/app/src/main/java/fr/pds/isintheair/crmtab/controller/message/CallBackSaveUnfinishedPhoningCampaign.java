fa52d849ea705313d50ba91eb227e720261e2502
tlacouque <titouanlac77@gmail.com>
2016-04-29 11:01:52 +0200

    #US Contact Later : Add comments on rest response

--- android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/controller/message/CallBackSaveUnfinishedPhoningCampaign.java
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/controller/message/CallBackSaveUnfinishedPhoningCampaign.java
@@ -8,6 +8,7 @@ import retrofit.Retrofit;
 
 /**
  * Created by tlacouque on 12/04/2016.
+ * Class used to save an unfinished phoning campaign
  */
 public class CallBackSaveUnfinishedPhoningCampaign implements Callback<ResponseRestPhoningCampaign> {
 
@@ -18,6 +19,11 @@ public class CallBackSaveUnfinishedPhoningCampaign implements Callback<ResponseR
         this.fragment = fragment;
     }
 
+    /**
+     * If there is no error on the response, it stop the campaign. If there is errors it called on failure method
+     * @param response
+     * @param retrofit
+     */
     @Override
     public void onResponse(Response<ResponseRestPhoningCampaign> response, Retrofit retrofit) {
         boolean bool = false;
@@ -29,6 +35,11 @@ public class CallBackSaveUnfinishedPhoningCampaign implements Callback<ResponseR
         fragment.stopCampaign(bool);
     }
 
+    /**
+     * Called when there is an error on the rest call/response.
+     * It call the method stopCampaign from the fragment pass in parameter.
+     * @param t
+     */
     @Override
     public void onFailure(Throwable t) {
         fragment.stopCampaign(false);
