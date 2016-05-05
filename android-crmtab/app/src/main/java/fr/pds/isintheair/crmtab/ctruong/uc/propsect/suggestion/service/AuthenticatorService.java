450206d56dce897fdc517a8e1dc8224ce074d6bc
ctruong <cminh.truong@gmail.com>
2016-05-05 02:52:44 +0200

    #Synchro UC  - Set up service

--- android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/ctruong/uc/propsect/suggestion/service/AuthenticatorService.java
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/ctruong/uc/propsect/suggestion/service/AuthenticatorService.java
@@ -1,13 +1,16 @@
 package fr.pds.isintheair.crmtab.ctruong.uc.propsect.suggestion.service;
 
+import android.accounts.Account;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
+import fr.pds.isintheair.crmtab.ctruong.uc.propsect.suggestion.account.AccountGeneral;
 import fr.pds.isintheair.crmtab.ctruong.uc.propsect.suggestion.account.Authenticator;
 
 public class AuthenticatorService extends Service {
-
+    private static final String TAG = "GenericAccountService";
     private Authenticator authenticator;
     public AuthenticatorService() {
         authenticator = new Authenticator(this);
@@ -17,5 +20,21 @@ public class AuthenticatorService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         return authenticator.getIBinder();
+    }
+
+    public static Account getAccount(){
+        final String accountName = AccountGeneral.ACCOUNT_NAME;
+        return new Account(accountName, AccountGeneral.ACCOUNT_TYPE);
+    }
+
+    @Override
+    public void onCreate() {
+        Log.i(TAG, "Service created");
+        authenticator = new Authenticator(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG, "Service destroyed");
     }
 }
