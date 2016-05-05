f61c53469ffb4f58cdc6809b745b12844285706e
ctruong <cminh.truong@gmail.com>
2016-05-05 02:43:08 +0200

    #Synchro UC  - Set up service

--- android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/ctruong/uc/propsect/suggestion/service/SyncService.java
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/ctruong/uc/propsect/suggestion/service/SyncService.java
@@ -3,6 +3,7 @@ package fr.pds.isintheair.crmtab.ctruong.uc.propsect.suggestion.service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 import fr.pds.isintheair.crmtab.ctruong.uc.propsect.suggestion.adapter.SyncAdapter;
 
@@ -15,7 +16,7 @@ public class SyncService extends Service {
     private static SyncAdapter sSyncAdapter = null;
     // Object to use as a thread-safe lock
     private static final Object sSyncAdapterLock = new Object();
-
+    private static final String TAG = "SyncService";
     /**
      * Instantiate the sync adapter object.
      */
@@ -33,6 +34,16 @@ public class SyncService extends Service {
             }
         }
     }
+
+    @Override
+    /**
+     * Logging-only destructor.
+     */
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "Service destroyed");
+    }
+
 
     /**
      * Return an object that allows the system to invoke
