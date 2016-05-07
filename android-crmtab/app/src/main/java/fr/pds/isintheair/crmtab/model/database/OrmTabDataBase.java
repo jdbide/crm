c5deb9f24dc30310d610d2aaa42e2c60ebd39d94
ctruong <cminh.truong@gmail.com>
2016-05-07 06:43:04 +0200

    #Synchro UC  - Set up service

--- android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/model/database/OrmTabDataBase.java
+++ android-crmtab/app/src/main/java/fr/pds/isintheair/crmtab/model/database/OrmTabDataBase.java
@@ -1,15 +1,26 @@
 package fr.pds.isintheair.crmtab.model.database;
 
+import android.net.Uri;
+
 import com.raizlabs.android.dbflow.annotation.Database;
+import com.raizlabs.android.dbflow.annotation.provider.ContentProvider;
 
 /**
  * Created by user on 08/12/2015.
  */
-
+@ContentProvider(authority = OrmTabDataBase.AUTHORITY,
+        database = OrmTabDataBase.class,
+        baseContentUri = OrmTabDataBase.BASE_CONTENT_URI)
 @Database(name = OrmTabDataBase.DBNAME, version = OrmTabDataBase.DBVERSION)
 public class OrmTabDataBase {
     public static final String DBNAME = "CRMTabDataBase";
     public static final int DBVERSION = 1;
+
+    // Define the authority of the provider
+    public static final String AUTHORITY = "fr.pds.isintheair.crm.controller.ctruong.uc.propsect.suggestion.provider";
+
+    public static final String BASE_CONTENT_URI = "content://";
+
 
 
 }
