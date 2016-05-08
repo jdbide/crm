482cf9a2dda40c63b90e1b1fc16571b45ee33370
ctruong <cminh.truong@gmail.com>
2016-05-08 20:50:07 +0200

    Merge branch 'dev' of http://192.168.20.3/isintheair/crm into uc-synchronisation
    # Conflicts:
    #	android-crmtab/app/src/main/AndroidManifest.xml

--- android-crmtab/app/src/test/java/fr/pds/isintheair/crmtab/SynchronisationActivityTest.java
+++ android-crmtab/app/src/test/java/fr/pds/isintheair/crmtab/SynchronisationActivityTest.java
@@ -1,24 +1,15 @@
 package fr.pds.isintheair.crmtab;
 
-import android.content.Context;
-import android.net.ConnectivityManager;
 import android.os.Binder;
-import android.os.Build;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.robolectric.RobolectricGradleTestRunner;
-import org.robolectric.RuntimeEnvironment;
-import org.robolectric.annotation.Config;
-import org.robolectric.internal.ShadowExtractor;
-import org.robolectric.shadows.ShadowConnectivityManager;
-import org.robolectric.shadows.ShadowNetworkInfo;
+
+import fr.pds.isintheair.crmtab.ctruong.uc.propsect.suggestion.view.activity.SynchronisationActivity;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import fr.pds.isintheair.crmtab.ctruong.uc.propsect.suggestion.view.activity.SynchronisationActivity;
 
 /**
  * Created by Truong on 5/8/2016.
