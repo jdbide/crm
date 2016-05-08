f8b538fa2a9e51118d8493a2efe83485096bda63
ctruong <cminh.truong@gmail.com>
2016-05-08 21:31:22 +0200

    Merge branch 'dev' of http://192.168.20.3/isintheair/crm into uc-synchronisation
    # Conflicts:
    #	android-crmtab/app/src/main/AndroidManifest.xml

---
+++ android-crmtab/app/src/test/java/fr/pds/isintheair/crmtab/SyncAdapterTest.java
@@ -0,0 +1,16 @@
+package fr.pds.isintheair.crmtab;
+
+import org.junit.Test;
+
+import fr.pds.isintheair.crmtab.ctruong.uc.propsect.suggestion.adapter.SyncAdapter;
+
+/**
+ * Created by Truong on 5/8/2016.
+ */
+public class SyncAdapterTest {
+    @Test
+    public void onPerformSyncTest() throws Exception {
+
+
+    }
+}
