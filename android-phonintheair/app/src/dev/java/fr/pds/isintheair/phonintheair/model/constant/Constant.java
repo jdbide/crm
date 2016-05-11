bade76d2613def933d15785aebffb07d4145bf97
jbide <bide26@gmail.com>
2016-05-11 18:11:00 +0200

    #US no ify presence : corrected rest url

--- android-phonintheair/app/src/dev/java/fr/pds/isintheair/phonintheair/model/constant/Constant.java
+++ android-phonintheair/app/src/dev/java/fr/pds/isintheair/phonintheair/model/constant/Constant.java
@@ -8,8 +8,8 @@ package fr.pds.isintheair.phonintheair.model.constant;
  ******************************************/
 
 public class Constant {
-    //public static final String BASE_URL = "http://192.168.20.3:8082/api/";
-    public static final String BASE_URL = "http://192.168.1.68:8080/server-rest/";
+    public static final String BASE_URL = "http://192.168.20.3:8082/api/";
+    //public static final String BASE_URL = "http://192.168.1.68:8080/server-rest/";
 
     public static String WEBSOCKET_CALL_ENDPOINT     = "ws://192.168.20.3:8084/call";
     //public static String WEBSOCKET_CALL_ENDPOINT     = "ws://192.168.1.1:8084/call";
