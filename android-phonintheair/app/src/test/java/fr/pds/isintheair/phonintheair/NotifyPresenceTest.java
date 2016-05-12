d56e075bd1a1f6c6b005973c6c59f72fb70bc875
jdatour <julien.datour@gmail.com>
2016-05-12 11:05:21 +0200

    Again a failing test

--- android-phonintheair/app/src/test/java/fr/pds/isintheair/phonintheair/NotifyPresenceTest.java
+++ android-phonintheair/app/src/test/java/fr/pds/isintheair/phonintheair/NotifyPresenceTest.java
@@ -39,42 +39,41 @@ import static junit.framework.TestCase.assertTrue;
  */
 public class NotifyPresenceTest {
 
-    final int TECH_NFC_A = 1;
-    final String EXTRA_NFC_A_SAK = "sak";    // short (SAK byte value)
+    final int    TECH_NFC_A       = 1;
+    final String EXTRA_NFC_A_SAK  = "sak";    // short (SAK byte value)
     final String EXTRA_NFC_A_ATQA = "atqa";  // byte[2] (ATQA value)
 
-    final int TECH_NFC_B = 2;
-    final String EXTRA_NFC_B_APPDATA = "appdata";    // byte[] (Application Data bytes from ATQB/SENSB_RES)
+    final int    TECH_NFC_B           = 2;
+    final String EXTRA_NFC_B_APPDATA  = "appdata";    // byte[] (Application Data bytes from ATQB/SENSB_RES)
     final String EXTRA_NFC_B_PROTINFO = "protinfo";  // byte[] (Protocol Info bytes from ATQB/SENSB_RES)
 
-    final int TECH_ISO_DEP = 3;
+    final int    TECH_ISO_DEP                = 3;
     final String EXTRA_ISO_DEP_HI_LAYER_RESP = "hiresp";  // byte[] (null for NfcA)
-    final String EXTRA_ISO_DEP_HIST_BYTES = "histbytes";  // byte[] (null for NfcB)
+    final String EXTRA_ISO_DEP_HIST_BYTES    = "histbytes";  // byte[] (null for NfcB)
 
-    final int TECH_NFC_F = 4;
-    final String EXTRA_NFC_F_SC = "systemcode";  // byte[] (system code)
+    final int    TECH_NFC_F      = 4;
+    final String EXTRA_NFC_F_SC  = "systemcode";  // byte[] (system code)
     final String EXTRA_NFC_F_PMM = "pmm";        // byte[] (manufacturer bytes)
 
-    final int TECH_NFC_V = 5;
+    final int    TECH_NFC_V             = 5;
     final String EXTRA_NFC_V_RESP_FLAGS = "respflags";  // byte (Response Flag)
-    final String EXTRA_NFC_V_DSFID = "dsfid";           // byte (DSF ID)
+    final String EXTRA_NFC_V_DSFID      = "dsfid";           // byte (DSF ID)
 
-    final int TECH_NDEF = 6;
-    final String EXTRA_NDEF_MSG = "ndefmsg";              // NdefMessage (Parcelable)
+    final int    TECH_NDEF            = 6;
+    final String EXTRA_NDEF_MSG       = "ndefmsg";              // NdefMessage (Parcelable)
     final String EXTRA_NDEF_MAXLENGTH = "ndefmaxlength";  // int (result for getMaxSize())
     final String EXTRA_NDEF_CARDSTATE = "ndefcardstate";  // int (1: read-only, 2: read/write, 3: unknown)
-    final String EXTRA_NDEF_TYPE = "ndeftype";            // int (1: T1T, 2: T2T, 3: T3T, 4: T4T, 101: MF Classic, 102: ICODE)
+    final String EXTRA_NDEF_TYPE      = "ndeftype";            // int (1: T1T, 2: T2T, 3: T3T, 4: T4T, 101: MF Classic, 102: ICODE)
 
     final int TECH_NDEF_FORMATABLE = 7;
 
     final int TECH_MIFARE_CLASSIC = 8;
 
-    final int TECH_MIFARE_ULTRALIGHT = 9;
+    final int    TECH_MIFARE_ULTRALIGHT          = 9;
     final String EXTRA_MIFARE_ULTRALIGHT_IS_UL_C = "isulc";  // boolean (true: Ultralight C)
 
-    final int TECH_NFC_BARCODE = 10;
+    final int    TECH_NFC_BARCODE               = 10;
     final String EXTRA_NFC_BARCODE_BARCODE_TYPE = "barcodetype";  // int (1: Kovio/ThinFilm)
-
 
 
     @Test
@@ -82,7 +81,7 @@ public class NotifyPresenceTest {
         Intent intent = new Intent();
 
         String action = intent.getAction();
-        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        Tag    tag    = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
 
         //String s = action + "\n\n" + tag.toString();
         String s = "";
@@ -98,12 +97,12 @@ public class NotifyPresenceTest {
                         if (recs[j].getTnf() == NdefRecord.TNF_WELL_KNOWN &&
                                 Arrays.equals(recs[j].getType(), NdefRecord.RTD_TEXT)) {
 
-                            byte[] payload = recs[j].getPayload();
+                            byte[] payload      = recs[j].getPayload();
                             String textEncoding = ((payload[0] & 0200) == 0) ? "UTF-8" : "UTF-16";
-                            int langCodeLen = payload[0] & 0077;
+                            int    langCodeLen  = payload[0] & 0077;
 
                             s = (new String(payload, langCodeLen + 1,
-                                    payload.length - langCodeLen - 1, textEncoding));
+                                            payload.length - langCodeLen - 1, textEncoding));
 
                             assertTrue(s == null);
 
@@ -111,7 +110,8 @@ public class NotifyPresenceTest {
                         }
                     }
                 }
-            } catch (Exception e) {
+            }
+            catch (Exception e) {
                 Log.e("TagDispatch", e.toString());
             }
 
@@ -119,7 +119,7 @@ public class NotifyPresenceTest {
     }
 
     @Test
-    public void testClockin(String idTag){
+    public void testClockin(String idTag) {
         //Interceptor
         HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
         // set your desired log level
@@ -132,21 +132,22 @@ public class NotifyPresenceTest {
         Gson gson = new GsonBuilder().disableHtmlEscaping().create();
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(Constant.BASE_URL)
+                .baseUrl(Constant.REST_URL)
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .client(httpClient)
                 .build();
 
-        Retrofit gen = new Retrofit.Builder().baseUrl(Constant.BASE_URL).addConverterFactory(GsonConverterFactory.create(gson)).build();
-        NotifyPresenceInterface service =  gen.create(NotifyPresenceInterface.class);
-        Call<ClockinObject> call = service.clockin(new ClockinObject(new User(), "entree"));
+        Retrofit                gen     = new Retrofit.Builder().baseUrl(Constant.REST_URL).addConverterFactory(GsonConverterFactory.create(gson)).build();
+        NotifyPresenceInterface service = gen.create(NotifyPresenceInterface.class);
+        Call<ClockinObject>     call    = service.clockin(new ClockinObject(new User(), "entree"));
 
         call.enqueue(new Callback<ClockinObject>() {
             @Override
             public void onResponse(Response<ClockinObject> response, Retrofit retrofit) {
                 if (response.isSuccess()) {
                     assertTrue(response.isSuccess());
-                } else {
+                }
+                else {
                     assertTrue(false);
 
                 }
@@ -157,6 +158,7 @@ public class NotifyPresenceTest {
             public void onFailure(Throwable t) {
                 assertTrue(false);
             }
-        });}
+        });
+    }
 }
 
